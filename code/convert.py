25c6014dae7b50d43cdfafa2fbfb2fc80f56f254
Alexander Hunt <alexander.hunt@ed.ac.uk>
2022-03-31 16:16:42 +0100

    update blur move

--- code/convert.py
+++ code/convert.py
@@ -142,3 +142,33 @@ def iterateBlur(path_to_folder='/Volumes/PHD/', start=0, end=100, step=5):
     for i in range(start, end, step):
         print('Threshold: ' + str(i))
         detectBlurr(path_to_folder, threshold=i)
+
+def detectAndMoveBlurr(path_to_folder='/Volumes/PHD/', threshold=100.0):
+    file = open(path_to_folder + 'recap.txt', "w")
+    file.write('Threshold: ' + str(threshold) + '\n')
+    print('Detecting blurr')
+    os.makedirs(path_to_folder + 'threshold_' + str(threshold))
+   # loop over the input images
+    for imagePath in paths.list_images(path_to_folder):
+        # load the image, convert it to grayscale, and compute the
+        # focus measure of the image using the Variance of Laplacian
+        # method
+        print("Checking image: " + imagePath)
+        image = cv2.imread(imagePath)
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        fm = variance_of_laplacian(gray)
+        # if the focus measure is less than the supplied threshold,
+        # then the image should be considered "blurry"
+        if fm < threshold:
+            print(imagePath)
+            # file.write(fm + "<" + threshold + '\n')
+            file.write('Below threshold' + imagePath + '\n')
+            shutil.move(imagePath, path_to_folder + 'threshold_' + str(threshold))
+            print("Moved " + imagePath)
+            file.write('Moved ' + imagePath + ' to folder' + '\n')
+    file.close()
+
+def iterateBlurMove(path_to_folder='/Volumes/PHD/', start=0, end=100, step=5):
+    for i in range(start, end, step):
+        print('Threshold: ' + str(i))
+        detectAndMoveBlurr(path_to_folder, threshold=i)
