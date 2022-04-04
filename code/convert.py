d7e19faddafa7c8aaf5b78ff65f786a4bfd4e201
Alexander Hunt <alexander.hunt@ed.ac.uk>
2022-04-04 14:38:19 +0100

    update

--- code/convert.py
+++ code/convert.py
@@ -1,3 +1,4 @@
+from turtle import st
 import cv2
 import glob, os, datetime
 from PIL import Image
@@ -142,11 +143,11 @@ def iterateBlur(path_to_folder='/Volumes/PHD/', start=0, end=100, step=5):
         print('Threshold: ' + str(i))
         detectBlurr(path_to_folder, threshold=i)
 
-def detectAndMoveBlurr(path_to_folder='/Volumes/PHD/', threshold=100.0):
-    file = open(path_to_folder + 'recap.txt', "w")
+def detectAndMoveBlurr(path_to_folder='/Volumes/PHD/', threshold=100.0, currentstep=110, outfolder='/Volumes/PHD/sorted/'):
+    file = open(outfolder + 'recap.txt', "w")
     file.write('Threshold: ' + str(threshold) + '\n')
     print('Detecting blurr')
-    os.makedirs(path_to_folder + 'threshold_' + str(threshold), exist_ok=True)
+    os.makedirs(outfolder + 'threshold_' + str(threshold), exist_ok=True)
    # loop over the input images
     for imagePath in paths.list_images(path_to_folder):
         # load the image, convert it to grayscale, and compute the
@@ -158,17 +159,19 @@ def detectAndMoveBlurr(path_to_folder='/Volumes/PHD/', threshold=100.0):
         fm = variance_of_laplacian(gray)
         # if the focus measure is less than the supplied threshold,
         # then the image should be considered "blurry"
-        if fm < threshold:
+        if fm < threshold & threshold < currentstep:
             print(imagePath)
             # file.write(fm + "<" + threshold + '\n')
             file.write('Below threshold' + imagePath + '\n')
-            shutil.copy(imagePath, path_to_folder + 'threshold_' + str(threshold))
-            os.rmtree(imagePath)
+            shutil.copy(imagePath, outfolder + 'threshold_' + str(threshold))
+            os.remove(imagePath)
             print("Moved " + imagePath)
             file.write('Moved ' + imagePath + ' to folder' + '\n')
+
     file.close()
 
-def iterateBlurMove(path_to_folder='/Volumes/PHD/', start=0, end=100, step=5):
+def iterateBlurMove(path_to_folder='/Volumes/PHD/', outfolder='/Volumes/PHD/sorted/', start=0, end=100, step=5):
     for i in range(start, end, step):
         print('Threshold: ' + str(i))
-        detectAndMoveBlurr(path_to_folder, threshold=i)
+        currentstep = str(range) + str(step)
+        detectAndMoveBlurr(path_to_folder, threshold=i, currentstep=currentstep, outfolder=outfolder)
