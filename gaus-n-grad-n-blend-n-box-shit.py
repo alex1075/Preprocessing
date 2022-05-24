876fd136338d96c4c7c3b85029432a17f3b69729
Alexander Hunt <alexander.hunt@ed.ac.uk>
2022-05-24 18:38:40 +0100

    autolabel semi working

--- gaus-n-grad-n-blend-n-box-shit.py
+++ gaus-n-grad-n-blend-n-box-shit.py
@@ -6,10 +6,17 @@ from matplotlib import pyplot as plt
 imag = cv2.imread('functions - WIP/test_1.jpg',0)
 Blu = cv2.medianBlur(imag,5)
 
-sob = cv2.adaptiveThreshold(Blu,255,cv2.ADAPTIVE_THRESH_MEAN_C,\
-            cv2.THRESH_BINARY,11,2)
+sobelx = cv2.Sobel(Blu,cv2.CV_64F,1,0,ksize=5)
+sobely = cv2.Sobel(Blu,cv2.CV_64F,0,1,ksize=5)
 
-plt.subplot(2,1,1),plt.imshow(imag,cmap = 'gray')
+sa = cv2.addWeighted(sobelx,1,sobely,1,0)
+sar = np.uint8(sa)
+sb = cv2.adaptiveThreshold(Blu,255,cv2.ADAPTIVE_THRESH_MEAN_C,\
+            cv2.THRESH_BINARY,11,2)
+sbr = np.uint8(sb)
+sob = cv2.addWeighted(sar,1,sbr,1,0)
+
+plt.subplot(2,1,1),plt.imshow(sob,cmap = 'gray')
 plt.title('Original'), plt.xticks([]), plt.yticks([])
 # plt.subplot(2,2,3),plt.imshow(Blu,cmap = 'gray')
 # plt.title('Median Blur'), plt.xticks([]), plt.yticks([])
