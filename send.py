b18a84a91edc1e9421d71162dd25d7454738718e
Alexander Hunt <alexander.hunt@ed.ac.uk>
2022-07-06 17:38:00 +0100

    could be onto something here

--- send.py
+++ send.py
@@ -40,3 +40,35 @@ with open(filename, "rb") as f:
         progress.update(len(bytes_read))
 # close the socket
 s.close()
+
+def send_file(path, file, server, port):
+    """
+    Send a file to a server.
+    """
+    # get the file size
+    filesize = os.path.getsize(path)
+    # create the client socket
+    s = socket.socket()
+    print(f"[+] Connecting to {server}:{port}")
+    
+    # connect to the server
+    s.connect((server, port))
+    print("[+] Connected.")
+    # send the filename and filesize
+    # start sending the file
+    progress = tqdm.tqdm(range(filesize), f"Sending {filename}", unit="B", unit_scale=True, unit_divisor=1024)
+    with open(filename, "rb") as f:
+        while True:
+            # read the bytes from the file
+            bytes_read = f.read(BUFFER_SIZE)
+            if not bytes_read:
+                # file transmitting is done
+                break
+            # we use sendall to assure transimission in 
+            # busy networks
+            s.sendall(bytes_read)
+            # update the progress bar
+            progress.update(len(bytes_read))
+    # close the socket
+    s.close()
+    print(f"[+] File {file} sent to {server}:{port}")
