9244c915e1d31acf2e3caa00e817e70d0e676507
Alexander Hunt <alexander.hunt@ed.ac.uk>
2022-07-07 14:10:09 +0100

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   code/helper/yolo.py

--- code/helper/yolo.py
+++ code/helper/yolo.py
@@ -1,30 +1,63 @@
 import os
+import tqdm
+from multiprocessing import Process
 
-class yolo:
-    """An attempt to incorporate darknet as a class"""
-    def dont_show(state):
-        if state == True:
-            return True
-        else:  
-            return False
-    
-    def mAP(state):
-        if state ==  True:
-            return True
-        else:
-            return False
-    
-    def __init__(self, weights_file, config_file, data_file, dont_show=True, mAP=True):
-        self.weights_file = weights_file
-        self.config_file = config_file
-        self.dont_show
-        self.data_file = data_file
-        self.train = 'cd .. && ./darknet/darknet detector train ' + self.data_file + ' ' + self.config_file + ' ' + self.weights_file 
-        self.valid = 'cd .. && ./darknet/darknet detector valid ' + self.data_file + ' ' + self.config_file + ' ' + self.weights_file
-        self.command = 'cd .. && ./darknet/darknet detector ' + self.data_file + ' ' + self.config_file + ' ' + self.weights_file
-        self.results_folder = '/mnt/c/Users/Alexander Hunt/results/'
-        self.result_OTH = '/mnt/C/Users/Alexander Hunt/results/comp4_det_test_OTR.txt'
-        self.result_PLT = '/mnt/C/Users/Alexander Hunt/results/comp4_det_test_PLT.txt'
-        self.result_RBC = '/mnt/C/Users/Alexander Hunt/results/comp4_det_test_RBC.txt'
-        self.result_RBC_sidew = '/mnt/C/Users/Alexander Hunt/results/comp4_det_test_RBC_sidew.txt'
-        self.result_RBC_overlap = '/mnt/C/Users/Alexander Hunt/results/comp4_det_test_RBC_overlap.txt'
+
+#         self.results_folder = '/mnt/c/Users/Alexander Hunt/results/'
+#         self.result_OTH = '/mnt/C/Users/Alexander Hunt/results/comp4_det_test_OTR.txt'
+#         self.result_PLT = '/mnt/C/Users/Alexander Hunt/results/comp4_det_test_PLT.txt'
+#         self.result_RBC = '/mnt/C/Users/Alexander Hunt/results/comp4_det_test_RBC.txt'
+#         self.result_RBC_sidew = '/mnt/C/Users/Alexander Hunt/results/comp4_det_test_RBC_sidew.txt'
+#         self.result_RBC_overlap = '/mnt/C/Users/Alexander Hunt/results/comp4_det_test_RBC_overlap.txt'
+def yolo_train(weights_file='cfg/yolov4.conv.137', config_file='cfg/yolov4.cfg', data_file='data/obj.data', GPU=1, dont_show=True, mAP=True):
+    args = ''
+    if dont_show == True:
+        args = ' --dont_show'
+    else:
+        pass
+    if mAP == True:
+        args = ' --map'
+    else:
+        pass
+    cfg = open(config_file, 'r')
+    for line in cfg:
+        if 'max_batches' in line:
+            lin = line.split(' = ')
+            max_batches = int(lin[1])
+    step = 0
+    output = os.system('cd .. && ./darknet/darknet detector train ' + data_file + ' ' + config_file + ' ' + weights_file + args)
+    progress = tqdm.tqdm(range(max_batches), f"Training, currently on step {step} of {max_batches}", unit="B", unit_scale=True, unit_divisor=1024)
+    for line in output:
+        if line[1:].isnumeric() == True:
+            step += 1
+            progress.update(step)
+            print(line)
+    # print(output)
+    return output
+
+def yolo_test(weights_file='cfg/yolov4.conv.137', config_file='cfg/yolov4.cfg', data_file='data/obj.data', GPU=1, dont_show=True, mAP=True):
+    args = ''
+    if dont_show == True:
+        args = ' --dont_show'
+    else:
+        pass
+    if mAP == True:
+        args = ' --map'
+    else:
+        pass
+    output = os.system('cd .. && ./darknet/darknet detector test ' + data_file + ' ' + config_file + ' ' + weights_file + args)
+    print(output)
+
+def yolo_valid(weights_file='cfg/yolov4.conv.137', config_file='cfg/yolov4.cfg', data_file='data/obj.data', GPU=1, dont_show=True, mAP=True):
+    args = ''
+    if dont_show == True:
+        args = ' --dont_show'
+    else:
+        pass
+    if mAP == True:
+        args = ' --map'
+    else:
+        pass
+    output = os.system('cd .. && ./darknet/darknet detector valid ' + data_file + ' ' + config_file + ' ' + weights_file + args)
+    print(output)
+
