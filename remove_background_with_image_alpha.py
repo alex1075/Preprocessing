85ede45e0cf55f3852fa4fa89a970a8758abd2c7
Alexander Hunt <alexander.hunt@ed.ac.uk>
2022-07-19 09:03:46 +0100

    update

--- remove_background_with_image_alpha.py
+++ remove_background_with_image_alpha.py
@@ -6,10 +6,11 @@ import numpy as np
 # remove backgroun of image using image of background
 def background_removal_with_img(original_img, bacground_img, alpha=0.5):
     img = cv2.imread(original_img)
+    # print(img)
     background = cv2.imread(bacground_img)
-    out = alpha * ( img -  background ) + (128, 128, 128)
+    # print(background)
+    out = (alpha * ( img -  background ) + 128).clip(0,255)
     return out
-
 
 
 # for i in np.arange(0, 4, 0.5):
@@ -18,4 +19,5 @@ def background_removal_with_img(original_img, bacground_img, alpha=0.5):
 #     cv2.imwrite('test_background_removed_alpha_' + str(i) + '.jpg', img)
 
 img = background_removal_with_img('data/test_1_416_416.jpg', 'backgrounds/background_416_416.jpg', 4)
-cv2.imwrite('test_background_removed_alpha_40.jpg', img)
+cv2.imwrite('test_background_removed_alpha_4.jpg', img)
+# print(img)
