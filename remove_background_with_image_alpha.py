a40d2b51078cba03b6727faa26590577be4273c7
Alexander Hunt <alexander.hunt@ed.ac.uk>
2022-07-19 10:44:41 +0100

    update

--- remove_background_with_image_alpha.py
+++ remove_background_with_image_alpha.py
@@ -4,20 +4,19 @@ import numpy as np
 
 
 # remove backgroun of image using image of background
-def background_removal_with_img(original_img, bacground_img, alpha=0.5):
+def background_removal_with_alpha(original_img, bacground_img, alpha=0.5):
     img = cv2.imread(original_img)
-    # print(img)
+    # print(img[0,0])
     background = cv2.imread(bacground_img)
-    # print(background)
+    # print(background[0,0])
     out = (alpha * ( img -  background ) + 128).clip(0,255)
+    # out = (alpha * ( img[0,0] -  background[0,0] ) + 128).clip(0,255)
+    out = np.around(out, decimals=0).clip(0,255)
+    out = out.astype(np.uint8)
     return out
 
 
-# for i in np.arange(0, 4, 0.5):
-#     print(i)
-#     img = background_removal_with_img('data/test_1_416_416.jpg', 'backgrounds/background_416_416.jpg', i)
-#     cv2.imwrite('test_background_removed_alpha_' + str(i) + '.jpg', img)
 
-img = background_removal_with_img('data/test_1_416_416.jpg', 'backgrounds/background_416_416.jpg', 4)
-cv2.imwrite('test_background_removed_alpha_4.jpg', img)
+img = background_removal_with_alpha('data/test_1_416_416.jpg', 'backgrounds/background_416_416.jpg', 1.5)
+cv2.imwrite('test_background_removed_alpha_test_nparound.jpg', img)
 # print(img)
