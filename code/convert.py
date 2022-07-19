b9e9390814d8b4a8465f962f986fa43b792fd125
Alexander Hunt <alexander.hunt@ed.ac.uk>
2022-07-19 17:01:52 +0100

    update

--- code/convert.py
+++ code/convert.py
@@ -186,3 +186,31 @@ def chopUpDataset(path_to_folder='/Users/alexanderhunt/Preprocessing/test_datase
     else:
         pass
     checkAllImg(outfolder, x, y)
+
+def batchBackgroundRemove(path_to_folder='output/', background_folder='background/', outfolder='/Volumes/PHD/removed/'):
+    return print('Not implemented yet')
+
+def batchBackgroundRemove(path_to_folder='output/', background_folder='backgrounds/', outfolder='data_2/', alpha=2):
+    list_img=[img for img in os.listdir(path_to_folder) if img.endswith('.jpg')==True]
+    list_background=[img for img in os.listdir(background_folder) if img.endswith('.jpg')==True]
+    list_txt=[img for img in os.listdir(path_to_folder) if img.endswith('.txt')==True]
+    for img in list_img:
+        index = img.split('_')
+        name = index[0] + '_' + index[1]
+        first_chop = index[2]
+        second_chop = index[3]
+        condition = 'background''_' + str(first_chop) + '_' + str(second_chop)
+        img_path = outfolder + img
+        background_element = [x for x in list_background if x==condition]
+        background = str(background_element[0])
+        img = background_removal_with_alpha(path_to_folder+img, background_folder+background, alpha)
+        cv2.imwrite(img_path, img)
+        print('Matched ' + img_path + ' with ' + background)
+        annotation_condition  = name + '_' + str(first_chop) + '_' + str(second_chop)[:-4] + '.txt'
+        annotation_file = [x for x in list_txt if x==annotation_condition]
+        annotation = str(annotation_file[0])
+        print('Matched ' + annotation + ' with ' + annotation_condition)
+        os.system('cp ' + path_to_folder + annotation + ' ' + outfolder + annotation)
+    classes_file = [x for x in list_txt if x=='classes.txt']
+    classes = str(classes_file[0])
+    os.system('cp ' + path_to_folder + classes + ' ' + outfolder + classes)
