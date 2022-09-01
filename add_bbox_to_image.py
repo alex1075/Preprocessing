44265c59874cee41efb952bb8007a51770232725
“Alexander <alexander.hunt@ed.ac.uk>
2022-09-01 13:12:44 +0000

    update

---
+++ add_bbox_to_image.py
@@ -0,0 +1,70 @@
+import os
+import cv2
+import decimal
+
+"""
+Classes:
+0 - OTR
+1 - PLT
+2 - RBC
+3 - RBC_overlap
+4 - RBC_sidew
+"""
+
+# Take image from list in directory and add rectangle using bbox coordinates 
+def add_bbox_to_image(image_path, classes, bbox_coordinates):
+    image = cv2.imread(image_path)
+    if classes == 0:
+        color = (0, 0, 255)
+    elif classes == 1:
+        color = (0, 255, 0)
+    elif classes == 2:
+        color = (255, 0, 0)
+    elif classes == 3:
+        color = (0, 0, 0)
+    elif classes == 4:
+        color = (255, 255, 0)
+    # elif classes == 5:
+    #     color = (0, 255, 255)
+    # else:
+    #     color = (255, 255, 255)
+    x1 = bbox_coordinates[0]
+    y1 = bbox_coordinates[1]
+    x2 = bbox_coordinates[2]
+    y2 = bbox_coordinates[3]
+    img = cv2.rectangle(image, (x1, y1), (x2, y2), color, 2)
+    # cv2.imshow('image', image)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+    return img
+
+def iterate_over_images(list, path_to_images, save_directory):
+    fill = open(list, 'r')
+    for line in fill:
+        # print(file)
+        print(line)
+        lin = line.split(' ')
+        # print(lin)
+        image = lin[0]
+        confidence = str(lin[1])
+        x1 = decimal.Decimal(lin[2])
+        y1 = decimal.Decimal(lin[3])
+        x2 = decimal.Decimal(lin[4])
+        y2 = decimal.Decimal(lin[5])
+        x1 = decimal.Decimal(x1 * 416)
+        y1 = decimal.Decimal(y1 * 416)
+        x2 = decimal.Decimal(x2 * 416)
+        y2 = decimal.Decimal(y2 * 416)
+        x1 = round(x1)
+        y1 = round(y1)
+        x2 = round(x2)
+        y2 = round(y2)
+        print(image)
+        print(confidence)
+        bbox_coordinates = [x1, y1, x2, y2]  
+        print(bbox_coordinates)
+        img = add_bbox_to_image(path_to_images + image+ '.jpg', int(confidence), bbox_coordinates)
+        cv2.imwrite(save_directory + image + '.jpg', img)
+
+
+iterate_over_images('/home/as-hunt/Preprocessing/predictons.txt', '/home/as-hunt/Preprocessing/image_bboxes/', '/home/as-hunt/Preprocessing/image_bboxes/')
--- make_prediction_file.py
+++ make_prediction_file.py
@@ -1,11 +1,11 @@
 import os
 import decimal
 
-file_weights = 'yolov4_new_data_1.weights'
-os.system('cd .. && ./darknet/darknet detector valid data/obj.data cfg/yolov4.cfg Saved_weights/'+file_weights)
+file_weights = 'NoSidesNoBG_3.weights'
+# os.system('cd .. && ./darknet/darknet detector valid /home/as-hunt/Etra-Space/obj.data /home/as-hunt/Etra-Space/cfg/yolov4.cfg Saved_weights/'+file_weights+' -gpus 0')
 
 p_file = open('predictons.txt', 'w')
-results_folder = '/mnt/c/Users/Alexander Hunt/results/'
+results_folder = '/home/as-hunt/results/'
 for file in os.listdir(results_folder):
     if file.endswith('.txt'):
         count = 0
@@ -19,7 +19,7 @@ for file in os.listdir(results_folder):
                         lin = line.split(' ')
                         # print(lin)
                         image = lin[0]
-                        confidence = str(lin[1])
+                        confidence = decimal.Decimal(lin[1])
                         x1 = decimal.Decimal(lin[2])
                         y1 = decimal.Decimal(lin[3])
                         x2 = decimal.Decimal(lin[4])
@@ -28,7 +28,7 @@ for file in os.listdir(results_folder):
                         y1 = decimal.Decimal(y1 / 416)
                         x2 = decimal.Decimal(x2 / 416)
                         y2 = decimal.Decimal(y2 / 416)
-                        p_file.write(image + ' ' + str(classes) + ' ' + str(x1) + ' ' + str(y1) + ' ' + str(x2) + ' ' + str(y2) + ' \n')
+                        p_file.write(image + ' ' + str(classes) + ' ' + str(x1) + ' ' + str(y1) + ' ' + str(x2) + ' ' + str(y2) + ' ' + str(confidence) + ' \n')
                         count += 1
                         print('Line ' + str(count) + ': ' + line)
                         # print(annotation) 
@@ -50,7 +50,7 @@ for file in os.listdir(results_folder):
                         y1 = decimal.Decimal(y1 / 416)
                         x2 = decimal.Decimal(x2 / 416)
                         y2 = decimal.Decimal(y2 / 416)
-                        p_file.write(image + ' ' + str(classes) + ' ' + str(x1) + ' ' + str(y1) + ' ' + str(x2) + ' ' + str(y2) +  ' \n')
+                        p_file.write(image + ' ' + str(classes) + ' ' + str(x1) + ' ' + str(y1) + ' ' + str(x2) + ' ' + str(y2) +  ' ' + str(confidence) + ' \n')
                         count += 1
                         print('Line ' + str(count) + ': ' + line)
                         # print(annotation) 
@@ -63,7 +63,7 @@ for file in os.listdir(results_folder):
                         lin = line.split(' ')
                         # print(lin)
                         image = lin[0]
-                        confidence = str(lin[1])
+                        confidence = decimal.Decimal(lin[1])
                         x1 = decimal.Decimal(lin[2])
                         y1 = decimal.Decimal(lin[3])
                         x2 = decimal.Decimal(lin[4])
@@ -72,7 +72,7 @@ for file in os.listdir(results_folder):
                         y1 = decimal.Decimal(y1 / 416)
                         x2 = decimal.Decimal(x2 / 416)
                         y2 = decimal.Decimal(y2 / 416)
-                        p_file.write(image + ' ' + str(classes) + ' ' + str(x1) + ' ' + str(y1) + ' ' + str(x2) + ' ' + str(y2) + ' \n')
+                        p_file.write(image + ' ' + str(classes) + ' ' + str(x1) + ' ' + str(y1) + ' ' + str(x2) + ' ' + str(y2) + ' ' + str(confidence) + ' \n')
                         count += 1
                         print('Line ' + str(count) + ': ' + line)
                         # print(annotation) 
@@ -85,7 +85,7 @@ for file in os.listdir(results_folder):
                         lin = line.split(' ')
                         # print(lin)
                         image = lin[0]
-                        confidence = str(lin[1])
+                        confidence = decimal.Decimal(lin[1])
                         x1 = decimal.Decimal(lin[2])
                         y1 = decimal.Decimal(lin[3])
                         x2 = decimal.Decimal(lin[4])
@@ -94,7 +94,7 @@ for file in os.listdir(results_folder):
                         y1 = decimal.Decimal(y1 / 416)
                         x2 = decimal.Decimal(x2 / 416)
                         y2 = decimal.Decimal(y2 / 416)
-                        p_file.write(image + ' ' + str(classes) + ' ' + str(x1) + ' ' + str(y1) + ' ' + str(x2) + ' ' + str(y2) + ' \n')
+                        p_file.write(image + ' ' + str(classes) + ' ' + str(x1) + ' ' + str(y1) + ' ' + str(x2) + ' ' + str(y2) + ' ' + str(confidence) + ' \n')
                         count += 1
                         print('Line ' + str(count) + ': ' + line)
                         # print(annotation) 
@@ -107,7 +107,7 @@ for file in os.listdir(results_folder):
                         lin = line.split(' ')
                         # print(lin)
                         image = lin[0]
-                        confidence = str(lin[1])
+                        confidence = decimal.Decimal(lin[1])
                         x1 = decimal.Decimal(lin[2])
                         y1 = decimal.Decimal(lin[3])
                         x2 = decimal.Decimal(lin[4])
@@ -116,7 +116,7 @@ for file in os.listdir(results_folder):
                         y1 = decimal.Decimal(y1 / 416)
                         x2 = decimal.Decimal(x2 / 416)
                         y2 = decimal.Decimal(y2 / 416)
-                        p_file.write(image + ' ' + str(classes) + ' ' + str(x1) + ' ' + str(y1) + ' ' + str(x2) + ' ' + str(y2) + ' \n')
+                        p_file.write(image + ' ' + str(classes) + ' ' + str(x1) + ' ' + str(y1) + ' ' + str(x2) + ' ' + str(y2) + ' ' + str(confidence) + ' \n')
                         count += 1
                         print('Line ' + str(count) + ': ' + line)
                         # print(annotation) 
